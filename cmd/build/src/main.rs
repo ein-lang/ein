a4a700c83c2fafe1ddcaff1da2f224226b3cfa5e
Yota Toyama <raviqqe@gmail.com>
2019-10-22 23:10:00 -0700

    Refactor build command

--- cmd/build/src/main.rs
+++ cmd/build/src/main.rs
@@ -1,6 +1,5 @@
-mod build;
-
-use build::build;
+use std::io::Write;
+use std::path::Path;
 
 fn main() {
     if let Err(error) = run() {
@@ -16,7 +15,46 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
         );
     }
 
-    build(std::env::var("SLOTH_ROOT")?)?;
+    let root_directory = std::env::var("SLOTH_ROOT")?;
+
+    run_command(
+        std::process::Command::new(Path::new(&root_directory).join("target/release/sloth-compile"))
+            .arg("-s")
+            .arg(".")
+            .arg("-o")
+            .arg(".")
+            .arg("main.sl"),
+    )?;
+
+    run_command(
+        std::process::Command::new("clang")
+            .arg("-O3")
+            .arg("-flto")
+            .arg("-ldl")
+            .arg("-lpthread")
+            .arg("main.bc")
+            .arg(Path::new(&root_directory).join("target/release/libruntime.a")),
+    )?;
 
     Ok(())
+}
+
+fn run_command(command: &mut std::process::Command) -> Result<(), std::io::Error> {
+    let output = command.output()?;
+
+    if output.status.success() {
+        return Ok(());
+    }
+
+    std::io::stderr().write_all(&output.stdout)?;
+    std::io::stderr().write_all(&output.stderr)?;
+
+    Err(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        output
+            .status
+            .code()
+            .map(|code| format!("a command exited with status code {}", code))
+            .unwrap_or_else(|| "a command exited with no status code".into()),
+    ))
 }
