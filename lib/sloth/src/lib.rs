fefdbf0564f98dfef2fb5882e541dab83227598e
Yota Toyama <raviqqe@gmail.com>
2019-10-24 21:21:28 -0700

    Add functions to serialize/desrialize module interfaces

--- lib/sloth/src/lib.rs
+++ lib/sloth/src/lib.rs
@@ -13,3 +13,15 @@ pub mod types;
 pub use compile::{compile, ModuleObject};
 pub use parse::{parse_module, parse_module_path, Source};
 pub use path::ModulePath;
+
+pub fn serialize_module_interface(
+    module_interface: &ast::ModuleInterface,
+) -> Result<Vec<u8>, serde_json::Error> {
+    Ok(serde_json::to_string(module_interface)?.as_bytes().into())
+}
+
+pub fn deserialize_module_interface(
+    data: &[u8],
+) -> Result<ast::ModuleInterface, serde_json::Error> {
+    Ok(serde_json::from_slice(data)?)
+}
