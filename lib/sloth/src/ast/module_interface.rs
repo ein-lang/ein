1b12a7c0d87009c6703f27bdd714d30b7c5654f1
Yota Toyama <raviqqe@gmail.com>
2019-11-01 23:33:40 -0700

    Refactor serialize module

--- lib/sloth/src/ast/module_interface.rs
+++ lib/sloth/src/ast/module_interface.rs
@@ -1,8 +1,9 @@
 use crate::path::ModulePath;
 use crate::types::Type;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct ModuleInterface {
     path: ModulePath,
     types: HashMap<String, Type>,
