3449d02998b0f736d2badbc8dfc64ad4bf03b1a6
Yota Toyama <raviqqe@gmail.com>
2020-01-01 00:22:01 +0000

    Fix const struct

--- lib/llvm/src/constants.rs
+++ lib/llvm/src/constants.rs
@@ -1,6 +1,7 @@
 use super::type_::*;
 use super::value::*;
 use llvm_sys::core::*;
+use std::os::raw::c_uint;
 
 pub fn const_int(int_type: Type, value: u64) -> Value {
     unsafe { LLVMConstInt(int_type.into(), value, 0) }.into()
@@ -8,6 +9,21 @@ pub fn const_int(int_type: Type, value: u64) -> Value {
 
 pub fn const_real(real_type: Type, value: f64) -> Value {
     unsafe { LLVMConstReal(real_type.into(), value) }.into()
+}
+
+pub fn const_named_struct(type_: Type, values: &[Value]) -> Value {
+    unsafe {
+        LLVMConstNamedStruct(
+            type_.into(),
+            values
+                .iter()
+                .map(|value| value.into())
+                .collect::<Vec<_>>()
+                .as_mut_ptr(),
+            values.len() as c_uint,
+        )
+    }
+    .into()
 }
 
 pub fn get_undef(type_: Type) -> Value {
