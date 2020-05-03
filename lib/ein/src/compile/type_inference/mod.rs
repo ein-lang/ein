cefa31f35a9f9cb8f5f5e49f0252771b73910bb3
Yota Toyama <raviqqe@gmail.com>
2020-05-03 19:59:33 +0000

    Add type inference test

--- lib/ein/src/compile/type_inference/mod.rs
+++ lib/ein/src/compile/type_inference/mod.rs
@@ -1259,6 +1259,44 @@ mod tests {
 
             assert_eq!(infer_types(&module), Ok(module));
         }
+
+        #[test]
+        fn infer_types_of_record_element_operations() {
+            let record_type = types::Record::new(
+                "Foo",
+                vec![(
+                    "foo".into(),
+                    types::None::new(SourceInformation::dummy()).into(),
+                )]
+                .into_iter()
+                .collect(),
+                SourceInformation::dummy(),
+            );
+
+            let module = Module::from_definitions_and_type_definitions(
+                vec![TypeDefinition::new("Foo", record_type.clone())],
+                vec![ValueDefinition::new(
+                    "x",
+                    RecordElementOperation::new(
+                        types::Reference::new("Foo", SourceInformation::dummy()),
+                        "foo",
+                        RecordConstruction::new(
+                            types::Reference::new("Foo", SourceInformation::dummy()),
+                            vec![("foo".into(), None::new(SourceInformation::dummy()).into())]
+                                .into_iter()
+                                .collect(),
+                            SourceInformation::dummy(),
+                        ),
+                        SourceInformation::dummy(),
+                    ),
+                    types::None::new(SourceInformation::dummy()),
+                    SourceInformation::dummy(),
+                )
+                .into()],
+            );
+
+            assert_eq!(infer_types(&module), Ok(module));
+        }
     }
 
     mod operation {
