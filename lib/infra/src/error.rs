9c1616222c2369941bce7a99539746762d1b5231
Yota Toyama <raviqqe@gmail.com>
2020-06-06 20:03:53 -0700

    Sort enum members

--- lib/infra/src/error.rs
+++ lib/infra/src/error.rs
@@ -3,10 +3,10 @@ use std::fmt::Display;
 
 #[derive(Debug, PartialEq)]
 pub enum InfrastructureError {
+    CommandExit { status_code: Option<i32> },
     HostNotFound,
     LlvmLinkNotFound,
     OriginUrlNotFound,
-    CommandExit { status_code: Option<i32> },
 }
 
 impl Error for InfrastructureError {}
@@ -14,15 +14,15 @@ impl Error for InfrastructureError {}
 impl Display for InfrastructureError {
     fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         match self {
-            Self::HostNotFound => write!(formatter, "host name for package name not defined"),
-            Self::LlvmLinkNotFound => write!(formatter, "llvm-link not found"),
-            Self::OriginUrlNotFound => write!(formatter, "repository origin URL not defined"),
             Self::CommandExit { status_code } => match status_code {
                 Some(status_code) => {
                     write!(formatter, "command exited with status code {}", status_code)
                 }
                 None => write!(formatter, "command exited without status code"),
             },
+            Self::HostNotFound => write!(formatter, "host name for package name not defined"),
+            Self::LlvmLinkNotFound => write!(formatter, "llvm-link not found"),
+            Self::OriginUrlNotFound => write!(formatter, "repository origin URL not defined"),
         }
     }
 }
