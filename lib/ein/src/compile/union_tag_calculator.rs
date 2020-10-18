2cd62e77346020d7cdeefdaf30c65d28f9317d80
Yota Toyama <raviqqe@gmail.com>
2020-10-17 17:52:06 -0700

    Refactor transformers (#237)
    * Calculate list type tags
    
    * Fix union tag calculation
    
    * Compile list types
    
    * Fix linting

--- lib/ein/src/compile/union_tag_calculator.rs
+++ lib/ein/src/compile/union_tag_calculator.rs
@@ -18,25 +18,21 @@ impl UnionTagCalculator {
     }
 
     pub fn calculate(&self, type_: &Type) -> Result<u64, CompileError> {
-        let type_ = self.reference_type_resolver.resolve(type_)?;
+        Ok(self.convert_type_id_to_tag(&self.calculate_type_id(&type_)?))
+    }
 
-        Ok(match type_ {
-            Type::Boolean(_)
-            | Type::Function(_)
-            | Type::List(_)
-            | Type::None(_)
-            | Type::Number(_)
-            | Type::Record(_) => self.convert_type_id_to_tag(&self.calculate_type_id(&type_)?),
-            Type::Any(_)
-            | Type::Reference(_)
-            | Type::Union(_)
-            | Type::Unknown(_)
-            | Type::Variable(_) => unreachable!(),
-        })
+    fn convert_type_id_to_tag(&self, type_id: &str) -> u64 {
+        // TODO Use safer Hasher.
+        let mut hasher = DefaultHasher::new();
+
+        type_id.hash(&mut hasher);
+
+        hasher.finish()
     }
 
     fn calculate_type_id(&self, type_: &Type) -> Result<String, CompileError> {
         Ok(match self.reference_type_resolver.resolve(type_)? {
+            Type::Any(_) => "Any".into(),
             Type::Boolean(_) => "Boolean".into(),
             Type::Function(function) => format!(
                 "({}->{})",
@@ -47,20 +43,75 @@ impl UnionTagCalculator {
             Type::None(_) => "None".into(),
             Type::Number(_) => "Number".into(),
             Type::Record(record) => record.name().into(),
-            Type::Any(_)
-            | Type::Reference(_)
-            | Type::Union(_)
-            | Type::Unknown(_)
-            | Type::Variable(_) => unreachable!(),
+            Type::Union(union) => format!(
+                "({})",
+                union
+                    .types()
+                    .iter()
+                    .map(|type_| self.calculate_type_id(type_))
+                    .collect::<Result<Vec<_>, _>>()?
+                    .join("|")
+            ),
+            Type::Reference(_) | Type::Unknown(_) | Type::Variable(_) => unreachable!(),
         })
     }
+}
 
-    fn convert_type_id_to_tag(&self, type_id: &str) -> u64 {
-        // TODO Use safer Hasher.
-        let mut hasher = DefaultHasher::new();
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::*;
+    use crate::debug::SourceInformation;
+    use crate::types;
 
-        type_id.hash(&mut hasher);
+    #[test]
+    fn calculate_none_list_type_id() {
+        assert_eq!(
+            UnionTagCalculator::new(ReferenceTypeResolver::new(&Module::dummy()))
+                .calculate_type_id(
+                    &types::List::new(
+                        types::None::new(SourceInformation::dummy()),
+                        SourceInformation::dummy()
+                    )
+                    .into()
+                ),
+            Ok("[None]".into())
+        );
+    }
 
-        hasher.finish()
+    #[test]
+    fn calculate_any_list_type_id() {
+        assert_eq!(
+            UnionTagCalculator::new(ReferenceTypeResolver::new(&Module::dummy()))
+                .calculate_type_id(
+                    &types::List::new(
+                        types::Any::new(SourceInformation::dummy()),
+                        SourceInformation::dummy()
+                    )
+                    .into()
+                ),
+            Ok("[Any]".into())
+        );
+    }
+
+    #[test]
+    fn calculate_union_list_type_id() {
+        assert_eq!(
+            UnionTagCalculator::new(ReferenceTypeResolver::new(&Module::dummy()))
+                .calculate_type_id(
+                    &types::List::new(
+                        types::Union::new(
+                            vec![
+                                types::Number::new(SourceInformation::dummy()).into(),
+                                types::None::new(SourceInformation::dummy()).into(),
+                            ],
+                            SourceInformation::dummy()
+                        ),
+                        SourceInformation::dummy()
+                    )
+                    .into()
+                ),
+            Ok("[(None|Number)]".into())
+        );
     }
 }
