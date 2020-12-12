a1b5cf851fb844b2b6b5b48cee56552632bcd26d
Yota Toyama <raviqqe@gmail.com>
2020-12-12 21:54:44 +0000

    Rename module

--- lib/ein/src/parse/mod.rs
+++ lib/ein/src/parse/mod.rs
@@ -1,9 +1,9 @@
 #[macro_use]
 mod attempt;
 mod error;
-mod parse;
+mod parse_module;
 mod parsers;
 mod utilities;
 
 pub use error::ParseError;
-pub use parse::parse;
+pub use parse_module::parse;
