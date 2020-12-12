75838a66793acab6507975d6e5a4117feecf6425
Yota Toyama <raviqqe@gmail.com>
2020-12-12 21:49:01 +0000

    Separate parse module

---
+++ lib/ein/src/parse/parse.rs
@@ -0,0 +1,198 @@
+use super::error::ParseError;
+use super::parsers::{module, stream};
+use crate::ast;
+use combine::Parser;
+
+pub fn parse(source_content: &str, source_name: &str) -> Result<ast::UnresolvedModule, ParseError> {
+    Ok(module()
+        .parse(stream(source_content, source_name))
+        .map(|(module, _)| module)
+        .map_err(|error| ParseError::new(source_name, &error))?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::*;
+    use crate::debug::SourceInformation;
+    use crate::path::*;
+    use crate::types;
+    use indoc::indoc;
+
+    #[test]
+    fn parse_function_definition() {
+        assert_eq!(
+            parse("foo : Number -> Number -> Number\nfoo x y = 42", ""),
+            Ok(UnresolvedModule::from_definitions(vec![
+                FunctionDefinition::new(
+                    "foo",
+                    vec!["x".into(), "y".into()],
+                    Number::new(42.0, SourceInformation::dummy()),
+                    types::Function::new(
+                        types::Number::new(SourceInformation::dummy()),
+                        types::Function::new(
+                            types::Number::new(SourceInformation::dummy()),
+                            types::Number::new(SourceInformation::dummy()),
+                            SourceInformation::dummy(),
+                        ),
+                        SourceInformation::dummy(),
+                    ),
+                    SourceInformation::dummy(),
+                )
+                .into()
+            ]))
+        );
+    }
+
+    #[test]
+    fn parse_let_expression_with_single_definition() {
+        assert_eq!(
+            parse("x : Number\nx = (let x = 42\nin x)", ""),
+            Ok(UnresolvedModule::from_definitions(vec![
+                VariableDefinition::new(
+                    "x",
+                    Let::new(
+                        vec![VariableDefinition::new(
+                            "x",
+                            Number::new(42.0, SourceInformation::dummy()),
+                            types::Unknown::new(SourceInformation::dummy()),
+                            SourceInformation::dummy(),
+                        )
+                        .into()],
+                        Variable::new("x", SourceInformation::dummy()),
+                        SourceInformation::dummy(),
+                    ),
+                    types::Number::new(SourceInformation::dummy()),
+                    SourceInformation::dummy(),
+                )
+                .into()
+            ]))
+        );
+    }
+
+    #[test]
+    fn parse_let_expression_with_multiple_definitions() {
+        assert_eq!(
+            parse(
+                indoc!(
+                    "
+                    main : Number -> Number
+                        main x = (
+                            let
+                                f x = x
+                                y =
+                                f x
+                            in
+                                y
+                        )
+                        "
+                ),
+                ""
+            ),
+            Ok(UnresolvedModule::from_definitions(vec![
+                FunctionDefinition::new(
+                    "main",
+                    vec!["x".into()],
+                    Let::new(
+                        vec![
+                            FunctionDefinition::new(
+                                "f",
+                                vec!["x".into()],
+                                Variable::new("x", SourceInformation::dummy()),
+                                types::Unknown::new(SourceInformation::dummy()),
+                                SourceInformation::dummy(),
+                            )
+                            .into(),
+                            VariableDefinition::new(
+                                "y",
+                                Application::new(
+                                    Variable::new("f", SourceInformation::dummy()),
+                                    Variable::new("x", SourceInformation::dummy()),
+                                    SourceInformation::dummy(),
+                                ),
+                                types::Unknown::new(SourceInformation::dummy()),
+                                SourceInformation::dummy(),
+                            )
+                            .into(),
+                        ],
+                        Variable::new("y", SourceInformation::dummy()),
+                        SourceInformation::dummy(),
+                    ),
+                    types::Function::new(
+                        types::Number::new(SourceInformation::dummy()),
+                        types::Number::new(SourceInformation::dummy()),
+                        SourceInformation::dummy(),
+                    ),
+                    SourceInformation::dummy(),
+                )
+                .into()
+            ]))
+        );
+    }
+
+    #[test]
+    fn parse_with_import_statement() {
+        assert_eq!(
+            parse(
+                indoc!(
+                    "
+                    import \"package/Module\"
+
+                    main : Number -> Number
+                    main x = x
+                    "
+                ),
+                ""
+            ),
+            Ok(UnresolvedModule::new(
+                Export::new(Default::default()),
+                vec![UnresolvedImport::new(ExternalUnresolvedModulePath::new(
+                    vec!["package".into(), "Module".into()]
+                ))],
+                vec![],
+                vec![FunctionDefinition::new(
+                    "main",
+                    vec!["x".into()],
+                    Variable::new("x", SourceInformation::dummy()),
+                    types::Function::new(
+                        types::Number::new(SourceInformation::dummy()),
+                        types::Number::new(SourceInformation::dummy()),
+                        SourceInformation::dummy()
+                    ),
+                    SourceInformation::dummy()
+                )
+                .into()]
+            ))
+        );
+    }
+
+    #[test]
+    fn parse_with_comment() {
+        assert_eq!(
+            parse(
+                indoc!(
+                    "
+                    # foo is good
+                    foo : Number -> Number
+                    foo x = 42
+                    "
+                ),
+                ""
+            ),
+            Ok(UnresolvedModule::from_definitions(vec![
+                FunctionDefinition::new(
+                    "foo",
+                    vec!["x".into()],
+                    Number::new(42.0, SourceInformation::dummy()),
+                    types::Function::new(
+                        types::Number::new(SourceInformation::dummy()),
+                        types::Number::new(SourceInformation::dummy()),
+                        SourceInformation::dummy()
+                    ),
+                    SourceInformation::dummy()
+                )
+                .into()
+            ]))
+        );
+    }
+}
