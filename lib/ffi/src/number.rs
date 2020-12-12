30f38a776616bc16650722ad995036161290b81d
Yota Toyama <raviqqe@gmail.com>
2020-12-12 20:48:18 +0000

    Add number module

---
+++ lib/ffi/src/number.rs
@@ -0,0 +1,21 @@
+pub struct Number {
+    value: f64,
+}
+
+impl Number {
+    pub fn new(value: f64) -> Self {
+        Self { value }
+    }
+}
+
+impl From<Number> for f64 {
+    fn from(number: Number) -> Self {
+        number.value
+    }
+}
+
+impl From<f64> for Number {
+    fn from(value: f64) -> Self {
+        Self { value }
+    }
+}
