72e3c97bb27d93ad83b75a852c2af13f0df20ac6
Yota Toyama <raviqqe@gmail.com>
2020-12-15 02:56:16 +0000

    Fix main function's type

--- lib/runtime/src/main_.rs
+++ lib/runtime/src/main_.rs
@@ -4,12 +4,13 @@ use super::system::{System, SYSTEM};
 use std::os::raw::{c_int, c_void};
 
 extern "C" {
-    static ein_main: extern "C" fn(environment: *const c_void, argument: *const System) -> f64;
+    static ein_main:
+        extern "C" fn(environment: *const c_void, argument: *const System) -> ffi::Number;
 }
 
 #[no_mangle]
 pub extern "C" fn main() -> c_int {
     ffi::initialize();
 
-    (unsafe { ein_main(std::ptr::null(), &SYSTEM) }) as c_int
+    f64::from(unsafe { ein_main(std::ptr::null(), &SYSTEM) }) as c_int
 }
