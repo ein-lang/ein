09cf1857c658a43c2b9b12c4d5bb9ba25033b505
Yota Toyama <raviqqe@gmail.com>
2020-12-27 20:04:03 +0000

    Revert "Add _ein_fd_write builtin function"
    This reverts commit 8adf6a5c91fd674a13fb955678f178b29c4ed5ca.

--- lib/runtime/src/main_.rs
+++ lib/runtime/src/main_.rs
@@ -1,14 +1,16 @@
 #![cfg(not(test))]
 
+use super::system::{System, SYSTEM};
 use std::os::raw::{c_int, c_void};
 
 extern "C" {
-    static ein_main: extern "C" fn(environment: *const c_void) -> ffi::Number;
+    static ein_main:
+        extern "C" fn(environment: *const c_void, argument: *const System) -> ffi::Number;
 }
 
 #[no_mangle]
 pub extern "C" fn main() -> c_int {
     ffi::initialize();
 
-    f64::from(unsafe { ein_main(std::ptr::null()) }) as c_int
+    f64::from(unsafe { ein_main(std::ptr::null(), &SYSTEM) }) as c_int
 }
--- lib/runtime/src/system.rs
+++ lib/runtime/src/system.rs
@@ -1,12 +1,24 @@
+#![cfg(not(test))]
+
 use std::fs::File;
 use std::io::Write;
 use std::os::raw::c_void;
 use std::os::unix::io::FromRawFd;
 
-#[no_mangle]
-static _ein_fd_write: ffi::Closure = ffi::Closure::new(fd_write as *const c_void, 2);
+#[repr(C)]
+pub struct System {
+    fd_write: *const ffi::Closure,
+}
 
-extern "C" fn fd_write(
+unsafe impl Sync for System {}
+
+static FD_WRITE: ffi::Closure = ffi::Closure::new(system_fd_write as *const c_void, 2);
+
+pub static SYSTEM: System = System {
+    fd_write: &FD_WRITE,
+};
+
+extern "C" fn system_fd_write(
     _environment: *const c_void,
     fd: ffi::Number,
     buffer: ffi::EinString,
