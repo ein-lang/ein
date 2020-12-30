b9fbc42adbcdc097af4e799ec0715e3c2ce6d80a
Yota Toyama <raviqqe@gmail.com>
2020-12-30 00:28:38 +0000

    Remove bdwgc from FFI library

--- lib/ffi/src/lib.rs
+++ lib/ffi/src/lib.rs
@@ -1,12 +1,9 @@
-mod allocator;
 mod boolean;
 mod closure;
 mod none;
 mod number;
 mod string;
 
-#[cfg(not(test))]
-pub use allocator::initialize;
 pub use boolean::*;
 pub use closure::*;
 pub use none::*;
