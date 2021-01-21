bedffb3fbc66a4acaaa5be22d5b928d0c6b3d582
Yota Toyama <raviqqe@gmail.com>
2021-01-21 03:47:57 +0000

    Use C as backend

--- lib/lang/src/compile/mod.rs
+++ lib/lang/src/compile/mod.rs
@@ -165,18 +165,15 @@ pub fn compile(
     );
 
     Ok((
-        ssf_llvm::compile(
+        fmm_c::compile(&ssf_fmm::compile(
             &ModuleCompiler::new(
                 expression_compiler,
                 type_compiler,
                 configuration.string_type_configuration.clone(),
             )
             .compile(&module)?,
-            ssf_llvm::CompileConfiguration {
-                malloc_function_name: configuration.malloc_function_name.clone(),
-            }
-            .into(),
-        )?,
+        ))
+        .into(),
         ModuleInterfaceCompiler::new().compile(&module)?,
     ))
 }
