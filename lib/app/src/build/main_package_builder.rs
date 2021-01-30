6abdc9dcdf0fa3ab556e3d5775025b51da3c236e
Yota Toyama <raviqqe@gmail.com>
2021-01-30 01:52:43 +0000

    Refactor MainPackageBuilder

--- lib/app/src/build/main_package_builder.rs
+++ lib/app/src/build/main_package_builder.rs
@@ -3,8 +3,7 @@ use super::external_packages_downloader::ExternalPackagesDownloader;
 use super::package_builder::PackageBuilder;
 use super::package_configuration_reader::PackageConfigurationReader;
 use super::prelude_package_builder::PreludePackageBuilder;
-use crate::common::FilePath;
-use crate::common::Target;
+use crate::common::{CommandTarget, FilePath, PackageConfiguration, Target};
 use crate::infra::{CommandLinker, Logger};
 
 pub struct MainPackageBuilder<'a> {
@@ -41,6 +40,19 @@ impl<'a> MainPackageBuilder<'a> {
     pub fn build(&self) -> Result<(), Box<dyn std::error::Error>> {
         let package_configuration = self.package_configuration_reader.read(&FilePath::empty())?;
 
+        match package_configuration.build_configuration().target() {
+            Target::Command(command_target) => {
+                self.build_command(&package_configuration, command_target)
+            }
+            Target::Library => self.build_library(&package_configuration),
+        }
+    }
+
+    fn build_command(
+        &self,
+        package_configuration: &PackageConfiguration,
+        command_target: &CommandTarget,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let (prelude_package_object_file_paths, prelude_module_interfaces) =
             self.prelude_package_builder.build()?;
 
@@ -58,22 +70,40 @@ impl<'a> MainPackageBuilder<'a> {
             &prelude_module_interfaces,
         )?;
 
-        match package_configuration.build_configuration().target() {
-            Target::Command(command_target) => {
-                self.logger
-                    .log(&format!("linking command {}", command_target.name()))?;
+        self.logger
+            .log(&format!("linking command {}", command_target.name()))?;
 
-                self.command_linker.link(
-                    &prelude_package_object_file_paths
-                        .into_iter()
-                        .chain(external_module_object_file_paths)
-                        .chain(module_object_file_paths)
-                        .collect::<Vec<_>>(),
-                    command_target.name(),
-                )?;
-            }
-            Target::Library => {}
-        }
+        self.command_linker.link(
+            &prelude_package_object_file_paths
+                .into_iter()
+                .chain(external_module_object_file_paths)
+                .chain(module_object_file_paths)
+                .collect::<Vec<_>>(),
+            command_target.name(),
+        )?;
+
+        Ok(())
+    }
+
+    fn build_library(
+        &self,
+        package_configuration: &PackageConfiguration,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let (_, prelude_module_interfaces) = self.prelude_package_builder.build()?;
+
+        let external_package_configurations = self
+            .external_packages_downloader
+            .download(&package_configuration)?;
+
+        let (_, external_module_interfaces) = self
+            .external_packages_builder
+            .build(&external_package_configurations, &prelude_module_interfaces)?;
+
+        self.package_builder.build(
+            &package_configuration,
+            &external_module_interfaces,
+            &prelude_module_interfaces,
+        )?;
 
         Ok(())
     }
