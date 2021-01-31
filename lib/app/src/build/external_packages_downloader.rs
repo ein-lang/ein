f9a55b72f5bd4474b6877b927c250dcf5f46ad32
Yota Toyama <raviqqe@gmail.com>
2021-01-30 17:27:00 -0800

    Add cached external package downloader (#413)

--- lib/app/src/build/external_packages_downloader.rs
+++ lib/app/src/build/external_packages_downloader.rs
@@ -1,53 +1,18 @@
-use super::package_configuration_reader::PackageConfigurationReader;
-use crate::common::{ExternalPackage, FilePathResolver, PackageConfiguration};
-use crate::infra::{ExternalPackageDownloader, FileSystem, Logger};
+use super::cached_external_package_downloader::CachedExternalPackageDownloader;
+use crate::common::{ExternalPackage, PackageConfiguration};
 use std::collections::HashMap;
 
 pub struct ExternalPackagesDownloader<'a> {
-    package_configuration_reader: &'a PackageConfigurationReader<'a>,
-    external_package_downloader: &'a dyn ExternalPackageDownloader,
-    file_system: &'a dyn FileSystem,
-    file_path_resolver: &'a FilePathResolver<'a>,
-    logger: &'a dyn Logger,
+    cached_external_package_downloader: &'a CachedExternalPackageDownloader<'a>,
 }
 
 impl<'a> ExternalPackagesDownloader<'a> {
     pub fn new(
-        package_configuration_reader: &'a PackageConfigurationReader<'a>,
-        external_package_downloader: &'a dyn ExternalPackageDownloader,
-        file_system: &'a dyn FileSystem,
-        file_path_resolver: &'a FilePathResolver<'a>,
-        logger: &'a dyn Logger,
+        cached_external_package_downloader: &'a CachedExternalPackageDownloader<'a>,
     ) -> Self {
         Self {
-            package_configuration_reader,
-            external_package_downloader,
-            file_system,
-            file_path_resolver,
-            logger,
+            cached_external_package_downloader,
         }
-    }
-
-    pub fn download_one(
-        &self,
-        external_package: &ExternalPackage,
-    ) -> Result<PackageConfiguration, Box<dyn std::error::Error>> {
-        let directory_path = self
-            .file_path_resolver
-            .resolve_external_package_directory_path(&external_package);
-
-        if !self.file_system.exists(&directory_path) {
-            self.logger.log(&format!(
-                "downloading package {} {}",
-                external_package.name(),
-                external_package.version()
-            ))?;
-
-            self.external_package_downloader
-                .download(&external_package, &directory_path)?;
-        }
-
-        self.package_configuration_reader.read(&directory_path)
     }
 
     pub fn download(
@@ -57,7 +22,9 @@ impl<'a> ExternalPackagesDownloader<'a> {
         let mut package_configurations = HashMap::new();
 
         for external_package in external_packages {
-            let package_configuration = self.download_one(&external_package)?;
+            let package_configuration = self
+                .cached_external_package_downloader
+                .download(&external_package)?;
 
             package_configurations.extend(
                 self.download(
