12be62accbe0b386bc619147c80f066049756879
Yota Toyama <raviqqe@gmail.com>
2021-03-09 03:40:17 +0000

    Make Result type FFI-safe

--- lib/ffi/src/cps/result.rs
+++ lib/ffi/src/cps/result.rs
@@ -1,9 +1,11 @@
 #[repr(C)]
 #[derive(Clone, Default)]
-pub struct Result {}
+pub struct Result {
+    _private: [u8; 0],
+}
 
 impl Result {
     pub fn new() -> Self {
-        Self {}
+        Self { _private: [] }
     }
 }
