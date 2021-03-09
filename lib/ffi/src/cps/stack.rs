0415a4270523c527dad23bc0205426abf9e87356
Yota Toyama <raviqqe@gmail.com>
2021-03-09 03:22:37 +0000

    Add cps module

---
+++ lib/ffi/src/cps/stack.rs
@@ -0,0 +1,23 @@
+use std::alloc::{alloc, Layout};
+
+const INITIAL_CAPACITY: usize = 256;
+const DEFAULT_ALIGNMENT: usize = 8;
+
+#[repr(C)]
+pub struct Stack {
+    base_pointer: *mut u8,
+    size: usize,
+    capacity: usize,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Self {
+            base_pointer: unsafe {
+                alloc(Layout::from_size_align(INITIAL_CAPACITY, DEFAULT_ALIGNMENT).unwrap())
+            },
+            size: 0,
+            capacity: INITIAL_CAPACITY,
+        }
+    }
+}
--- lib/ffi/src/lib.rs
+++ lib/ffi/src/lib.rs
@@ -1,5 +1,6 @@
 mod boolean;
 mod closure;
+pub mod cps;
 mod none;
 mod number;
 mod string;
