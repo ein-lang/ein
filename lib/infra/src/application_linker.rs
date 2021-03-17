76a55d8a45486e869be9c42d909944020fc1817e
Yota Toyama <raviqqe@gmail.com>
2021-03-17 01:22:02 +0000

    Find llc nicely

--- lib/infra/src/application_linker.rs
+++ lib/infra/src/application_linker.rs
@@ -28,13 +28,17 @@ impl<'a> app::ApplicationLinker for ApplicationLinker<'a> {
             .iter()
             .map(|path| self.file_path_converter.convert_to_os_path(path))
             .partition::<Vec<_>, _>(|path| path.extension() == Some(std::ffi::OsStr::new("bc")));
+        let llc = which::which("llc-13")
+            .or_else(|_| which::which("llc-12"))
+            .or_else(|_| which::which("llc-11"))
+            .or_else(|_| which::which("llc"))?;
 
         for path in &bitcode_paths {
             // LLVM C API doesn't seem to support the tailcallopt pass directly.
             // So we compile each bitcode file with the pass manually in order
             // to optimize all tail calls.
             self.command_runner.run(
-                std::process::Command::new("llc")
+                std::process::Command::new(&llc)
                     .arg("-O3")
                     .arg("-tailcallopt")
                     .arg("-filetype=obj")
