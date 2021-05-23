06259cec28a83fbd6c4dd43dc91cf9c6262d5cd2
Yota Toyama <raviqqe@gmail.com>
2021-05-23 00:08:21 +0000

    Fix closure format

--- lib/ffi/src/closure.rs
+++ lib/ffi/src/closure.rs
@@ -1,9 +1,10 @@
-use std::os::raw::c_void;
+use std::{os::raw::c_void, ptr::null};
 
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct Closure {
     entry_pointer: *const c_void,
+    drop_function: *const u8,
     arity: usize,
 }
 
@@ -11,6 +12,7 @@ impl Closure {
     pub const fn new(entry_pointer: *const c_void, arity: usize) -> Self {
         Self {
             entry_pointer,
+            drop_function: null(),
             arity,
         }
     }
