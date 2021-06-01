751619b3a238b93c0f8e55cbf6bb045469204c29
Yota Toyama <raviqqe@gmail.com>
2021-06-01 02:01:46 +0000

    Add check_memory_leak.sh

--- tools/check_memory_leak_in_loop.sh
+++ tools/check_memory_leak_in_loop.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+. $(dirname $0)/utilities.sh
+
 valgrind --log-file=valgrind.log "$@" >/dev/null &
 pid=$!
 
@@ -10,6 +12,4 @@ sleep 5
 kill $pid
 wait $pid || :
 
-grep 'definitely lost: 0 bytes in 0 blocks' valgrind.log
-grep 'indirectly lost: 0 bytes in 0 blocks' valgrind.log
-grep '0 errors from 0 contexts' valgrind.log
+test_valgrind_log valgrind.log
