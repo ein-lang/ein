9b9a4ec30ba8ba5735ed73ca08d223dff336ce8e
Yota Toyama <raviqqe@gmail.com>
2021-06-08 15:48:17 +0000

    Fix app crate

--- lib/app/src/common/package_configuration/external_package.rs
+++ lib/app/src/common/package_configuration/external_package.rs
@@ -1,20 +1,30 @@
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct ExternalPackage {
     name: String,
+    url: String,
     // This version is the "expected" one different from lang::Package.
     version: String,
 }
 
 impl ExternalPackage {
-    pub fn new(name: impl Into<String>, version: impl Into<String>) -> Self {
+    pub fn new(
+        name: impl Into<String>,
+        url: impl Into<String>,
+        version: impl Into<String>,
+    ) -> Self {
         Self {
             name: name.into(),
+            url: url.into(),
             version: version.into(),
         }
     }
 
     pub fn name(&self) -> &str {
         &self.name
+    }
+
+    pub fn url(&self) -> &str {
+        &self.url
     }
 
     pub fn version(&self) -> &str {
