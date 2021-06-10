779b0495aecc6cea9a41a586fad123d943200002
Yota Toyama <raviqqe@gmail.com>
2021-06-10 02:36:43 +0000

    Refactor locations

--- lib/lang/src/debug/location.rs
+++ lib/lang/src/debug/location.rs
@@ -21,12 +21,4 @@ impl Location {
     pub fn column_number(&self) -> usize {
         self.column_number
     }
-
-    pub fn increment_line_number(&self) -> Self {
-        Self::new(self.line_number + 1, 1)
-    }
-
-    pub fn increment_column_number(&self) -> Self {
-        Self::new(self.line_number, self.column_number + 1)
-    }
 }
