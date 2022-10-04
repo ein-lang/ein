c43e6dc69c45601235edde4ab1ac8c80ab2fceaf
Yota Toyama <raviqqe@gmail.com>
2022-10-03 17:59:44 -0700

    Do not set PATH

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -10,4 +10,3 @@ brew install llvm@$llvm_version
 llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
 
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
-echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
